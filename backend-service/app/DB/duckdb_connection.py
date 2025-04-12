9a58164e20ca1a33b17df5dc32dc737528d9df0f
Akshat Jain <aj.xdadev@gmail.com>
2025-04-13 03:26:08 +0530

    Minor Bugfixing to fix index.db file read when hosted on docker

--- backend-service/app/DB/duckdb_connection.py
+++ backend-service/app/DB/duckdb_connection.py
@@ -1,13 +1,9 @@
 import duckdb
 import os
 
-DB_PATH = os.getenv("DB_PATH", "./index.db")
-#BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-#DB_PATH = os.getenv("DB_PATH", os.path.join(BASE_DIR, "index.db"))
+DB_PATH = os.getenv("DB_PATH", "app/index.db")
 
-#DB_PATH = "/app/index.db"
-#DB_PATH = "/Users/sancup/Index-Tracker-Assessment-AJ/backend-service/app/index.db"
 print(f"Using DuckDB at: {os.path.abspath(DB_PATH)}") #help local debug
-
+print(f" File exists? {os.path.exists(DB_PATH)}") #help local debug
 def get_connection():
     return duckdb.connect(database=DB_PATH, read_only=False)
